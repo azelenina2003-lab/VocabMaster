1f848c9e8b9d42a8e1cb0cefd7726e3a3ac875b2
Anastasia Zelenina <azelenina2003@gmail.com>
2026-03-16 11:42:16 +0300

    add factory

--- flashcard_project/flashcards/tests/factories.py
+++ flashcard_project/flashcards/tests/factories.py
@@ -1,5 +1,15 @@
 import factory
+from django.contrib.auth.models import User
 from flashcards.models import Category, Entry
+
+# новая фабрика для пользователя
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'user{n}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
+    password = factory.PostGenerationMethodCall('set_password', 'testpass123')
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
